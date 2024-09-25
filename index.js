600b718138d4ffa4778c63c1f7b89ca618c04905
ian.lee <ian.lee@cupix.com>
2024-09-25 13:39:46 +0900

    Update .gitignore and index.html, and optimize animation in index.js

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const map = new maplibregl.Map({
   container: "map",
   style:
     "https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL", // style URL
-  center: [-78.032, 38.913],
-  zoom: 7,
+  center: [-77.132, 37.413],
+  zoom: 7
 });
 
-// San Francisco
 const origin = map.getCenter().toArray();
 
 // Washington DC
@@ -23,60 +22,32 @@ const route = {
       type: "Feature",
       geometry: {
         type: "LineString",
-        coordinates: [origin, destination],
+        coordinates: [origin],
       },
     },
   ],
 };
 
-// A single point that animates along the route.
-// Coordinates are initially set to origin.
-const point = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      properties: {},
-      geometry: {
-        type: "Point",
-        coordinates: origin,
-      },
-    },
-  ],
+const fullRoute = {
+  type: "Feature",
+  geometry: {
+      type: "LineString",
+      coordinates: [origin, destination]
+  }
 };
 
-// Calculate the distance in kilometers between route start/end point.
-const lineDistance = turf.lineDistance(route.features[0], "kilometers");
+const lineDistance = turf.lineDistance(fullRoute, "kilometers");
 
-const arc = [];
+const steps = 100;
 
-// Number of steps to use in the arc and animation, more steps means
-// a smoother arc and animation, but too many steps will result in a
-// low frame rate
-const steps = 500;
+const incrementDistance = lineDistance / steps;
 
-// Draw an arc between the `origin` & `destination` of the two points
-for (let i = 0; i < lineDistance; i += lineDistance / steps) {
-  const segment = turf.along(route.features[0], i, "kilometers");
-  arc.push(segment.geometry.coordinates);
-}
-
-// Update the route with calculated arc coordinates
-route.features[0].geometry.coordinates = arc;
-
-// Used to increment the value of the point measurement against the route.
 let counter = 0;
 
 map.on("load", () => {
-  // Add a source and layer displaying a point which will be animated in a circle.
   map.addSource("route", {
     type: "geojson",
     data: route,
-  });
-
-  map.addSource("point", {
-    type: "geojson",
-    data: point,
   });
 
   map.addLayer({
@@ -85,70 +56,30 @@ map.on("load", () => {
     type: "line",
     paint: {
       "line-width": 2,
-      "line-color": "#007cbf",
-    },
-  });
-
-  map.addLayer({
-    id: "point",
-    source: "point",
-    type: "symbol",
-    layout: {
-      "icon-image": "airport_15",
-      "icon-rotate": ["get", "bearing"],
-      "icon-rotation-alignment": "map",
-      "icon-overlap": "always",
-      "icon-ignore-placement": true,
+      "line-color": "black",
     },
   });
 
   function animate() {
-    // Update point geometry to a new position based on counter denoting
-    // the index to access the arc.
-    point.features[0].geometry.coordinates =
-      route.features[0].geometry.coordinates[counter];
 
-    // Calculate the bearing to ensure the icon is rotated to match the route arc
-    // The bearing is calculate between the current point and the next point, except
-    // at the end of the arc use the previous point and the current point
-    point.features[0].properties.bearing = turf.bearing(
-      turf.point(
-        route.features[0].geometry.coordinates[
-          counter >= steps ? counter - 1 : counter
-        ]
-      ),
-      turf.point(
-        route.features[0].geometry.coordinates[
-          counter >= steps ? counter : counter + 1
-        ]
-      )
-    );
+    const currentSegment = turf.along(fullRoute, incrementDistance * counter,  "kilometers");
+    route.features[0].geometry.coordinates.push(currentSegment.geometry.coordinates);
 
-    // Update the source with this new data.
-    map.getSource("point").setData(point);
+    map.getSource("route").setData(route);
 
-    // Request the next frame of animation so long the end has not been reached.
     if (counter < steps) {
       requestAnimationFrame(animate);
     }
 
-    counter = counter + 1;
+    ++counter;
   }
 
   document.getElementById("replay").addEventListener("click", () => {
-    // Set the coordinates of the original point back to origin
-    point.features[0].geometry.coordinates = origin;
-
-    // Update the source layer
-    map.getSource("point").setData(point);
-
-    // Reset the counter
+    route.features[0].geometry.coordinates = [origin];
+    map.getSource("route").setData(route);
     counter = 0;
-
-    // Restart the animation.
-    animate(counter);
+    animate();
   });
 
-  // Start the animation.
-  animate(counter);
+  animate();
 });
